a29390ebf19191fdab869c046a83b79efc59212e
RaphaelBACPR <raphaelsfreitaswd@gmail.com>
2022-07-18 07:08:42 -0300

    erros ortograficos

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,9 +5,9 @@ const AboutContainer = () => {
 
     <section className="about-container">
       <h2>Sobre</h2>
-      <p>Apaixonado por tecnologia, aprendiz auto-dadata, com conhecimentos avançados, buscando uma primeira oportunidade no mercado para alavancar seu crescimento.</p>
-      <p>Vestibulando pela UFPR em Analise e Desenvolvimento de Sistemas, buscando o cresciemtno teorico e profissional que uma universidade renomada como a UFPR oferece.</p>
-      <p>SPA desevolvido por mim, utilizado React + SASS.</p>
+      <p>Apaixonado por tecnologia, aprendiz auto-didata, com conhecimentos avançados, buscando uma primeira oportunidade no mercado para alavancar seu crescimento.</p>
+      <p>Vestibulando pela UFPR em análise e Desenvolvimento de Sistemas, buscando o crescimento teórico e profissional que uma universidade renomada como a UFPR oferece.</p>
+      <p>SPA desenvolvido por mim, utilizado React + SASS.</p>
     </section>
   )
 }
